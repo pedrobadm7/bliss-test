ecd308a9c12f3c1da1c59858ba13cc50d0ed64c9
Pedro Barros <pedrobars7a@gmail.com>
2022-02-04 20:28:21 -0300

    create page header component

--- src/pages/NewQuestion/index.js
+++ src/pages/NewQuestion/index.js
@@ -1,3 +1,9 @@
+import PageHeader from '../../Components/PageHeader';
+
 export default function NewQuestion() {
-  return 'NewQuestion';
+  return (
+    <PageHeader
+      title="New Question"
+    />
+  );
 }
