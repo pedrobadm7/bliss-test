40c26e39f4553cbb5728d987893f0303a2b0515b
Pedro Barros <pedrobars7a@gmail.com>
2022-02-05 13:30:29 -0300

    showing errors on interface

--- src/Components/QuestionForm/index.js
+++ src/Components/QuestionForm/index.js
@@ -20,7 +20,7 @@ export default function QuestionForm({ buttonLabel }) {
     if (!event.target.value) {
       setErrors((prevState) => [
         ...prevState,
-        { field: 'question', message: 'This field must be filled' },
+        { field: 'question', message: 'This field must be filled with a question' },
       ]);
     } else {
       setErrors((prevState) => prevState.filter((error) => error.field !== 'question'));
@@ -50,14 +50,12 @@ export default function QuestionForm({ buttonLabel }) {
     if (!value) {
       setErrors((prevState) => [
         ...prevState,
-        { field: fieldName, message: 'This field must be filled' },
+        { field: fieldName, message: 'This field must be filled with a choice' },
       ]);
     } else {
       setErrors((prevState) => prevState.filter((error) => error.field !== fieldName));
     }
   }
-
-  console.log({ errors });
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -70,43 +68,52 @@ export default function QuestionForm({ buttonLabel }) {
     });
   }
 
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
+
   return (
     <S.Form onSubmit={handleSubmit}>
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('question')}>
         <Input
           placeholder="Question"
+          error={getErrorMessageByFieldName('question')}
           value={question}
           onChange={handleQuestionChange}
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('firstChoice')}>
         <Input
           placeholder="Choice 1"
+          error={getErrorMessageByFieldName('firstChoice')}
           value={firstChoice}
           onChange={(event) => handleChoiceChange(event, 'firstChoice')}
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('secondChoice')}>
         <Input
           placeholder="Choice 2"
+          error={getErrorMessageByFieldName('secondChoice')}
           value={secondChoice}
           onChange={(event) => handleChoiceChange(event, 'secondChoice')}
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('thirdChoice')}>
         <Input
           placeholder="Choice 3"
+          error={getErrorMessageByFieldName('thirdChoice')}
           value={thirdChoice}
           onChange={(event) => handleChoiceChange(event, 'thirdChoice')}
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('fourthChoice')}>
         <Input
           placeholder="Choice 4"
+          error={getErrorMessageByFieldName('fourthChoice')}
           value={fourthChoice}
           onChange={(event) => handleChoiceChange(event, 'fourthChoice')}
         />
