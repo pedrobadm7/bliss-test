cefe56255b35fcab9076193fbf0fe2fcb41d720c
Pedro Barros <pedrobars7a@gmail.com>
2022-02-06 18:32:17 -0300

    create health check to server

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -3,10 +3,19 @@ class HttpClient {
     this.baseURL = baseURL;
   }
 
-  async get(path) {
+  async get(path, serverHealthPath) {
+    const responseHealth = await fetch(`${this.baseURL}${serverHealthPath}`);
     const response = await fetch(`${this.baseURL}${path}`);
 
-    return response.json();
+    const body = await response.json();
+
+    if (responseHealth.ok && response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      `Server health check failed: ${responseHealth.status}`,
+    );
   }
 }
 
